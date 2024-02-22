b438f4edfd33204a02a70f17ed710387d1b6fd98
aperrom1709 <aperrom1709@g.educaand.es>
2024-02-22 11:42:34 +0100

    first commit

---
+++ ejercicio1/PrimeService/PrimeService.cs
@@ -0,0 +1,13 @@
+﻿namespace PrimeService;
+
+using System;
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            throw new NotImplementedException("Please create a test first.");
+        }
+    }
+}
